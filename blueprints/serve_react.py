36ae1c9110107040412240f53c09faacc63fd419
You Gao <djx3rn@virginia.edu>
2024-11-09 18:11:52 -0500

    feat: the field is set

--- blueprints/serve_react.py
+++ blueprints/serve_react.py
@@ -1,4 +1,4 @@
-from flask import Flask, send_from_directory, make_response, request
+from flask import Flask, send_from_directory, make_response, request, render_template
 from flask_smorest import Blueprint
 from flask.views import MethodView
 
@@ -8,22 +8,27 @@ import logging as log
 bp = Blueprint('forward', 'forward', url_prefix='/', description='Forwarding SendGrid Email Payloads')
 
 log = log.getLogger(__name__)
-react_build_path = r"C:\Users\commo\OneDrive - University of Virginia\School\STEM\CS\Coding Projects 2024\playground\react_apps\clock\build"
+react_build_path = r"C:\Users\commo\OneDrive - University of Virginia\School\STEM\CS\Coding Projects 2024\playground\react_apps"
 
 
 @bp.route('/')
 class ReactView(MethodView):
     def get(self):
-        return send_from_directory(react_build_path, "index.html")
+        return render_template('home.html')
     def post(self):
         log.info(request.json)
         return make_response('OK', 200)
-    
-@bp.route('/static/<string:type>/<string:file>')
+@bp.route('/<string:app>/')
+class ReactView(MethodView):
+    def get(self, app):
+        index_path = os.path.join(react_build_path, app, 'build')
+        return send_from_directory(index_path, 'index.html')
+
+@bp.route('/<string:app>/static/<string:type>/<string:file>')
 class StaticView(MethodView):
-    def get(self, type, file):
+    def get(self, app, type, file):
         print(type)
         print(file)
-        file_path = os.path.join(react_build_path, 'static', type)
+        file_path = os.path.join(react_build_path, app, 'build', 'static', type)
         print(file_path)
         return send_from_directory(file_path, file)
