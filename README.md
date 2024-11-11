bc406cb60e281164e94d73672568726de144565a
You Gao <djx3rn@virginia.edu>
2024-11-11 10:30:53 -0500

    feat: basic idea

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # playground
 as said above, playground for apps and ideas i have
+
+### Learning Notes
+* Remember to clean-up your component **always**
+* React is powerful because it manages hooks and states which are bundled into components
--- react_apps/mood/src/App.js
+++ react_apps/mood/src/App.js
@@ -1,23 +1,39 @@
 import logo from './logo.svg';
 import './App.css';
-
+import { useState, useEffect } from 'react';
+ 
 function App() {
+  const [color, setColor] = useState('black');
+
+  function changeColor() {
+    // js can use document to access the DOM
+    const mood = document.querySelector('input').value;
+    document.querySelector('input').value = '';
+    const mood_str = mood.toLowerCase();
+    console.log(mood)
+    setColor(mood_str);
+  }
+
+  function listenForEnter(e) {
+      if (e.key === 'Enter' && !e.repeat) {
+        changeColor();
+      }
+    }
+
+  useEffect(() => {
+    // listen to an enter key press in the input field
+    window.addEventListener('keydown', listenForEnter);
+
+    // clean up
+    return () => {
+      window.removeEventListener('keydown', listenForEnter);
+    }
+
+    }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" style={{background: color, width: "100%", height: "100%", position: "fixed"}}>
+      <input id="input_box" type="text" placeholder="Enter your mood" />
     </div>
   );
 }
